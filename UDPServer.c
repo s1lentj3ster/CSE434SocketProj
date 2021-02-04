c38224f754aeef9386c6a6e2d132329e2703cc01
Brian Moore <c0d3enigma@gmail.com>
2021-02-03 23:03:46 -0700

    Added Struct and Function placeholders

--- UDPServer.c
+++ UDPServer.c
@@ -6,8 +6,54 @@
 #include <unistd.h>     /* for close() */
 
 
+//Client Register Info Structure
+typedef struct{
+    char name_client [20];
+    char client_IP [20];
+    int client_socket;
+
+}Client_Info;
+
+//Client list Structure
+typedef struct{
+
+    Client_Info new_Client;    
+
+}Contact_List;
+
+
 int main(int argc, char *argv[])
 {
 
-    
+
+}
+
+//Register Function
+int register_Client(char client_contact, char client_IP, int client_Socket){
+
+}
+
+//Create Function
+int create_List(char client_Contact_List){
+
+}
+
+//Query-List Function
+void query_ClientList(){
+
+}
+
+//Join Function
+int join_List(char contact_List, char contact_Name){
+
+}
+
+//Leave Function
+void client_Leave(char contact_List, char contact_Name){
+
+}
+
+//Exit (Partial) Function
+void client_Exit(char contact_Name){
+
 }
