8dbf0201d5fcce054998622f994c6bd4d5da69a4
Brian Moore <c0d3enigma@gmail.com>
2021-02-03 22:01:37 -0700

    Project FIles - Rev 1

---
+++ UDPServer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>      /* for printf() and fprintf() */
+#include <sys/socket.h> /* for socket() and bind() */
+#include <arpa/inet.h>  /* for sockaddr_in and inet_ntoa() */
+#include <stdlib.h>     /* for atoi() and exit() */
+#include <string.h>     /* for memset() */
+#include <unistd.h>     /* for close() */
+
+
+int main(int argc, char *argv[])
+{
+
+    
+}
