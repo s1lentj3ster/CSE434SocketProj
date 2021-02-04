11c5be39189944c564f138273f2679c26909e976
Brian Moore <c0d3enigma@gmail.com>
2021-02-03 23:07:41 -0700

    Created util.h and moved struct from source to header

--- UDPServer.c
+++ UDPServer.c
@@ -6,20 +6,7 @@
 #include <unistd.h>     /* for close() */
 
 
-//Client Register Info Structure
-typedef struct{
-    char name_client [20];
-    char client_IP [20];
-    int client_socket;
 
-}Client_Info;
-
-//Client list Structure
-typedef struct{
-
-    Client_Info new_Client;    
-
-}Contact_List;
 
 
 int main(int argc, char *argv[])
---
+++ util.h
@@ -0,0 +1,14 @@
+//Client Register Info Structure
+typedef struct{
+    char name_client [20];
+    char client_IP [20];
+    int client_socket;
+
+}Client_Info;
+
+//Client list Structure
+typedef struct{
+
+    Client_Info new_Client;    
+
+}Contact_List;
