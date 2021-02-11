7d4027ed54f7d5b9cb00f48ae905b7421426300e
Brian Moore <c0d3enigma@gmail.com>
2021-02-11 01:42:58 -0700

    Attempt to fix querydata. In progress

--- SourceCode/joinleave.py
+++ SourceCode/joinleave.py
@@ -18,7 +18,10 @@ def join (command):
             #ok Im sure there is a more elegant way than this so please feel free to optimize it
             ip = databaseSet[contactName]["IP"]
             port = databaseSet[contactName]["port"]
-            contactList[listName].update({contactName : { "IP" : ip, "port" : port}})
+            contactList[listName] = {}
+            contactList[listName][contactName] = {}
+            contactList[listName][contactName]['IP'] = ip #.update({contactName : { "IP" : ip, "port" : port}})
+            contactList[listName][contactName]['port'] = port
             feedback = 'SUCCESS.\nYou have joined list '+listName+'\n'
     else:
         feedback = 'FAIL.\nYou are not registered, or '+listName+' does not exist, or your name is already in '+ listName+'.\n'
--- SourceCode/query.py
+++ SourceCode/query.py
@@ -5,10 +5,17 @@ def query_list ():
     #this is return code
     code = 0
     feedback = ''
-
+    
     for name, info in contactList.items():
-        feedback += name + '\n'
+        feedback += name + '\n' + info
         code +=  1
+        for cont in info:
+            feedback += info[cont]['IP'] + str(info[cont])
+            
+            
+                 
+           
+
     #this is only temporary, we will have 2 returns, but as of rn the client only reads 1 server message before quitting, but also it works so idk lol
     feedback = str(code)+ '\n'+ feedback.encode(encoding = 'utf-8')
     return feedback
