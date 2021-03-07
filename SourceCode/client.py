f12d586fe33b4279df2751dabb54631bffeaf940
Thy Do <thydo@asu.edu>
2021-03-07 04:01:12 -0700

    Add comments

--- SourceCode/client.py
+++ SourceCode/client.py
@@ -16,11 +16,8 @@ serverName = sys.argv[1]  # <--- Server IP address here
 serverPort = int(sys.argv[2])  # Server's port number
 clientSocket = socket(AF_INET, SOCK_DGRAM)  # Creates client's socket
 
-# Use Port 10005 for communication Client to Client (Peer to Peer)
+# Use Dynamic Port get for communication Client to Client (Peer to Peer)
 chat_Socket = socket(AF_INET, SOCK_DGRAM)
-
-# chat_Socket.bind(('', 10006))
-
 
 def message_thread():  # Client Listening for incomming messages.
     while True:
@@ -34,43 +31,49 @@ def message_thread():  # Client Listening for incomming messages.
         except OSError:
             break
     return    
-def message_processing(message, serverAdd):
+def message_processing(message, serverAdd): #Processes and prints incoming peer message
         listName = ''
         nextIP = ''
         nextPort = 0
         sendList = []
 
-        try:
+        try: #Decodes message list
             output = list(pickle.loads(message.decode('base64','strict')))
-            im_message = output[1]
-	    listName = output[0]           
-            contactList = output[2]
-            count = int(output[3]) - 1
+            im_message = output[1] # Peer Message
+	    listName = output[0]   # List Name       
+            contactList = output[2] # IP List 
+            count = int(output[3]) - 1 # Message TTL, decreases for every send
             print('Incoming: ' + im_message + ' from ' + serverAdd[0] + ' ' + str(serverAdd[1]))
 	    print ('List: ' + listName)
+	    # Decodes the list
             contactList = OrderedDict(pickle.loads(contactList.decode('base64', 'strict')))
             utils.print_list(contactList)
-
+	    # Rotates list to next peer, gets IP, Port 
             contactList = utils.rotate_values(contactList)
             nextName = list(contactList)[0]
             nextIP = contactList[nextName]['IP']
             nextPort = contactList[nextName]['port']
+            #Encodes list
             sendList = pickle.dumps(contactList).encode('base64', 'strict')
 	    return nextIP, nextPort, sendList, im_message, listName, count 
         except OSError:
     	    return
 
 def send_message(sendto_Host, sendto_Port, listIP, immess, listName, count):  # Send message from one client to another?
-       	    send = [immess, listName, listIP, count]
-       	    send = pickle.dumps(send).encode('base64', 'strict')
-       	    chat_Socket.sendto(send.encode(), (sendto_Host, int(sendto_Port)))
+       	    send = [immess, listName, listIP, count] #Wraps everything in one list
+       	    send = pickle.dumps(send).encode('base64', 'strict') #encode list
+       	    chat_Socket.sendto(send.encode(), (sendto_Host, int(sendto_Port))) #Send to next peer
             return
 
 if (serverPort < 10000 or serverPort > 10499):
     print('Please use port between 10000 and 10499\n')
     exit(1)
+
+#Initiates the chat socket listener     
 message_process = multiprocessing.Process(target=message_thread)
 message_process.start()
+
+#Client-Server interaction
 print('You have been connected to ' + serverName)
 while True:
     message = raw_input('Enter Command: \n')  # Send prompt to client #new line added to prevent issues with input
@@ -111,13 +114,11 @@ while True:
             nextIP = contactList[nextName]['IP']
             nextPort = contactList[nextName]['port']
             # print(nextPort)
-            # These 2 will need to be sent as separate messages, so the list can be decoded properly
             sendList = pickle.dumps(contactList).encode('base64', 'strict')  # <--- Encode list to send over
             im_message = raw_input('Enter Your IM Message: \n')  # <----- GET SENDER MESSAGE HERE
-            im_message = im_message
             
             count = str(len(contactList))
-            
+            #Calls sender function
             send_message(nextIP, nextPort, sendList, im_message, contact, count)
 
         else:
